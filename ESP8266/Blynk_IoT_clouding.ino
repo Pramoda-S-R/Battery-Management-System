edbde682352dd3e1f41780726a1c250bcf24bb37
Pramoda-S-R <145824616+Pramoda-S-R@users.noreply.github.com>
2023-09-28 11:58:41 +0530

    Update Blynk_IoT_clouding.ino

--- ESP8266/Blynk_IoT_clouding.ino
+++ ESP8266/Blynk_IoT_clouding.ino
@@ -1,4 +1,4 @@
-//ESP8266 CODE VERSION 1.3 READY FOR TESTING
+//ESP8266 CODE VERSION 1.4 READY FOR TESTING
 
 //defines need to be before header for smooth operation (DO NOT CHANGE THIS POSITION)
 #define BLYNK_TEMPLATE_ID   "TMPL30Jm3O2MQ"
@@ -14,9 +14,9 @@
 SoftwareSerial s(D6, D5);
 
 //declare and initialize sensor values
-int sensor_value1 = 0; // Variable to store Sensor Output
-int sensor_value2 = 0; // Variable to store Sensor Output
-int sensor_value3 = 0; // Variable to store Sensor Output
+float sensor_value1 = 0; // Variable to store Sensor Output
+float sensor_value2 = 0; // Variable to store Sensor Output
+float sensor_value3 = 0; // Variable to store Sensor Output
 
 //WiFi credentials
 char ssid[] = "Pramoda";
@@ -27,74 +27,50 @@ safeCurrent = 0;  //Update to actual safe value
 safeVoltage = 0;  //Update to actual safe value
 
 //alert bool declaration
-bool alertSent = false;
-
-// Blynk functions may take over here (TIMER INSTANCE MADE)
-BlynkTimer timer;
-
-//function call to write the value to virtual memories of Blynk
-void writetocloud()
-{
-  //updated ArduinoJson version 5 to version 6 
-  DynamicJsonDocument jsonDoc(1000);
-  DeserializationError error = deserializeJson(jsonDoc, s);
-  //checks for errors in JSON file
-  if (error) 
-  {
-    return;
-  }
-  //retrieves back data
-  JsonObject root = jsonDoc.as<JsonObject>();
-  //reads JSON file for specified entries
-  sensor_value1 = root["sensor1"];
-  sensor_value2 = root["sensor2"];
-  sensor_value3 = root["sensor3"];
-  //writes data to cloud virtual memory
-  Blynk.virtualWrite(V0, sensor_value1);
-  Blynk.virtualWrite(V1, sensor_value2);
-  Blynk.virtualWrite(V2, sensor_value3);
-}
+//bool alertSent = false;
 
 void setup() 
 {
-  // Initialize Serial port and SoftwareSerial port
+  // Start the Serial communication for debugging
   Serial.begin(115200);
-  s.begin(115200);
-  //connects to Blynk cloud and connects ESP to wifi
+  // Start the SoftwareSerial communication
+  s.begin(9600);
+  //Blynk commmunication
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  //calls writetocloud every 0.5 seconds (CUSTOMIZABLE)
-  timer.setInterval(500L, writetocloud);
-  //waits for serial connection
-  while (!Serial) continue;
-  // Work in progress... main components aren't included yet (ONLY PIN INITIALIZATIONS IF THEY EXIST)
-  
 }
 
 void loop() 
 {
-  //updated ArduinoJson version 5 to version 6 
-  DynamicJsonDocument jsonDoc(1000);
-  DeserializationError error = deserializeJson(jsonDoc, s);
-  //checks for errors in JSON file
-  if (error) 
-  {
-    return;
-  }
-  //retrieves back data
-  JsonObject root = jsonDoc.as<JsonObject>();
-  //reads JSON file for specified entries
-  sensor_value1 = root["sensor1"];
-  sensor_value2 = root["sensor2"];
-  sensor_value3 = root["sensor3"];
-  //serial monitor output
-  Serial.print("Current:");
-  Serial.print(sensor_value1);
-  Serial.print(", voltage:");
-  Serial.print(sensor_value2);
-  Serial.print(", Temp:");
-  Serial.println(sensor_value3);
+  Blynk.run();
+  // Check if there's data available to read
+  if (s.available()) {
+    // Read the incoming data
+    String json = s.readStringUntil('\n')
+    // Deserialize the JSON data
+    DynamicJsonDocument dat(100);
+    deserializeJson(dat, json);
+    // Extract the floating-point numbers
+    float sensor_value1 = dat["Current"];
+    float sensor_value2 = dat["Voltage"];
+    float sensor_value3 = dat["Temp"];
+    // Send the values to Blynk virtual pins
+    Blynk.virtualWrite(V1, sensor_value1);
+    Blynk.virtualWrite(V2, sensor_value2);
+    Blynk.virtualWrite(V3, sensor_value3);
 
+    // Calculate and send uptime to Blynk V3
+    long uptime = millis() / 1000;
+    Blynk.virtualWrite(V0, uptime);
+    //For Serial Monitor
+    Serial.print("Current: ");
+    Serial.println(sensor_value1);
+    Serial.print("Voltage: ");
+    Serial.println(sensor_value2);
+    Serial.print("Temperature: ");
+    Serial.println(sensor_value3);
+  }
   // Check if values exceed safe limits (ALSO NOT A BAD IDEA TO IMPLEMENT A ALERT PIN CHECK FROM BQ76925)
+  /*
   if (sensor_value1 > safeCurrent || sensor_value2 > safeVoltage || sensor_value3 >50)  //safevalues added but not set to actual values yet
   {
     if (!alertSent) {
@@ -104,8 +80,5 @@ void loop()
   } else {
     alertSent = false; // Reset alertSent when values are within safe limits
   }
-  
-  Blynk.run();
-  timer.run();
-  // Things may change here... (ALL CHANGES WERE MADE SO FAR)
+  */
 }
